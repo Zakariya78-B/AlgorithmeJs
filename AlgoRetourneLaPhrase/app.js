21fb086a4aa486802e5e5deff51a1d6d2cb93b6f
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-05 01:51:46 +0200

    commit retourne la phrase

---
+++ AlgoRetourneLaPhrase/app.js
@@ -0,0 +1,21 @@
+function retourneLaPhrase(str) {
+    str = str.split("");
+    str = str.reverse();
+    str = str.join("");
+    return str;
+}
+
+console.log(retourneLaPhrase("Bonjour Zak"));
+
+// ----------------- ÉNONCÉ -----------------
+
+// Créez un Algorithme qui va retourner une phrase. 
+// par exemple : "Bonjour à tous" donnera "suot à ruojnoB"
+// Bonne chance.
+
+
+
+
+
+
+
